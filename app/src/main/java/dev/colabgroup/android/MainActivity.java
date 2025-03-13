59bc2d0d6f5098a6b439b1f2e0f8bf855c499824
Edward Imanol Lopez Cruz <imanolopez3103@gmail.com>
2025-03-12 21:12:01 -0500

    Add button listener

--- app/src/main/java/dev/colabgroup/android/MainActivity.java
+++ app/src/main/java/dev/colabgroup/android/MainActivity.java
@@ -1,6 +1,9 @@
 package dev.colabgroup.android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        ImageButton view = findViewById(R.id.imageButton5);
+
+        view.setOnClickListener((v) -> {
+            Intent intent = new Intent(getApplicationContext(), Formulario_registro.class);
+
+            startActivity(intent);
         });
     }
 }
